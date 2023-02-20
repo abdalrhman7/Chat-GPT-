e4786e1fc26d8c88bcb263e2a2bc7936b98663b2
Abdalrhman Amr <119235456+abdalrhman7@users.noreply.github.com>
2023-02-21 00:09:08 +0200

    first commit

---
+++ lib/constants/api_const.dart
@@ -0,0 +1,2 @@
+String baseUrl = 'https://api.openai.com/v1';
+String apiKey = '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';
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:chat_gpt/view/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'constants/constants.dart';
+import 'controller/chats_provider.dart';
+import 'controller/models_provider.dart';
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitUp,
+  ]);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Color(0xFF343541),
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ModelsProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: scaffoldBackgroundColor,
+          appBarTheme: AppBarTheme(
+            color: cardColor,
+          ),
+        ),
+        home: const ChatScreen(),
+      ),
+    );
+  }
+}
